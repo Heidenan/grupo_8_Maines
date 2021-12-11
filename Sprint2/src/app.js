027f566499982076fc773247e28922af10f62c84
heidenan <jbradach26@gmail.com>
2021-12-11 14:06:52 -0300

    modifique ALGO

---
+++ Sprint2/src/app.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const path = require ('path')
+const app = express ()
+
+app.set("port", process.env.PORT || 3000)
+
+app.listen(app.get("port"), () =>
+    console.log("Servidor Corriendo") )
+
+
+app.get('/', (req,res) =>   res.sendFile(path.resolve(__dirname,'./views/index.html')))
+app.use(express.static(path.resolve(__dirname,'../public')))
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,'./views', 'register.html')))
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,'./views', 'login.html')))
+

5877030c6f7e1b9a43d2fe8b3ac58421ac02d9bf
Agustingmaggi <agustingmaggi@gmail.com>
2021-12-16 13:39:23 +0000

    subiendo hipervinculos a secciones en construccion para el proximo sprint

--- Sprint2/src/app.js
+++ Sprint2/src/app.js
@@ -15,4 +15,7 @@ app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,'./views',
 app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,'./views', 'login.html')))
 app.get('/carrito', (req,res) => res.sendFile(path.resolve(__dirname,'./views/carrito.html')))
 app.get('/entregas', (req,res) => res.sendFile(path.resolve(__dirname,'./views/entregas.html')))
+app.get('/compras', (req,res) => res.sendFile(path.resolve(__dirname,'./views/compras.html')))
+app.get('/ayuda', (req,res) => res.sendFile(path.resolve(__dirname,'./views/ayuda.html')))
+app.get('/suscripciones', (req,res) => res.sendFile(path.resolve(__dirname,'./views/suscripciones.html')))
 

873e8f128b4484840ab6a1b9f5d23ef38cd55600
Lucas Goñi <lucas.rgoni@gmail.com>
2022-01-10 19:05:32 -0300

    Commiteando para actualizar la rama

--- Sprint2/src/app.js
+++ Sprint2/src/app.js
@@ -1,22 +1,41 @@
-const express = require ('express')
-const path = require ('path')
-const app = express ()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-app.set("port", process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
 
 app.listen(app.get("port"), () =>
-    console.log("Servidor Corriendo en el puerto 3000") )
+  console.log("Servidor Corriendo en el puerto 3000")
+);
 
+app.use(express.static(path.resolve(__dirname, "../public")));
 
-app.use(express.static(path.resolve(__dirname,'../public')))
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/index.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views", "register.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views", "login.html"))
+);
+app.get("/carrito", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+);
+app.get("/entregas", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/entregas.html"))
+);
+app.get("/compras", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/compras.html"))
+);
+app.get("/ayuda", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/ayuda.html"))
+);
+app.get("/suscripciones", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/suscripciones.html"))
+);
+app.get("/detalleProducto", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
+);
 
-app.get('/', (req,res) =>   res.sendFile(path.resolve(__dirname,'./views/index.html')))
-app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,'./views', 'register.html')))
-app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,'./views', 'login.html')))
-app.get('/carrito', (req,res) => res.sendFile(path.resolve(__dirname,'./views/productCart.html')))
-app.get('/entregas', (req,res) => res.sendFile(path.resolve(__dirname,'./views/entregas.html')))
-app.get('/compras', (req,res) => res.sendFile(path.resolve(__dirname,'./views/compras.html')))
-app.get('/ayuda', (req,res) => res.sendFile(path.resolve(__dirname,'./views/ayuda.html')))
-app.get('/suscripciones', (req,res) => res.sendFile(path.resolve(__dirname,'./views/suscripciones.html')))
-app.get('/detalleProducto', (req,res) => res.sendFile(path.resolve(__dirname,'./views/productDetail.html')))
-
+// Prueba de sincronización

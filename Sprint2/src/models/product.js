fd4e0782c6e3756f980e0679af7edb90428f79df
Agustingmaggi <agustingmaggi@gmail.com>
2022-01-11 21:20:15 +0000

    sin estilos pero están, en teoria, todos los cambios para entregar. De todas maneras le falta mucho laburo para que quede bien

--- Sprint2/src/models/product.js
+++ Sprint2/src/models/product.js
@@ -1,6 +1,47 @@
 const path =require('path'); //nos ayuda a decir donde está el archivo (creo que el json de data)
 const fs = require('fs')    // nos ayuda a interactuar, leerlo, escribir, etc etc al archivo user de data
 const model = {
+    file: path.resolve(__dirname, '../data/products.json'),
+    read: () => fs.readFileSync(model.file),
+    write: data => fs.writeFileSync(model.file,JSON.stringify(data,null,2)),
+    all: () => JSON.parse(model.read()),
+    generate: data => Object({
+        id: model.all().length == 0 ? 1 : model.all().pop().id + 1,
+        name: data.name,
+        price: parseInt(data.price),
+        offert: data.offert ? true : false
+        /*aca hay que agregar todas las categorias que queremos que tenga cada objeto
+        O sea cada producto que vendemos por ejemplo */
+    }),
+    create: data => {
+        let newProduct = model.generate(data);
+        let all = model.all();
+        all.push(newProduct);
+        model.write(all)
+        return newProduct
+    },
+    search: (prop, value) => model.all().find(element => element[prop] == value),
+    update: (id,data) => {
+        let all = model.all();
+        let updated = all.map(e => {
+            if(e.id == id){
+                e.name = data.name;
+                e.price = data.price;
+                e.offert = data.offert ? true : false;
+                return e
+            }
+            return e
+        })
+        model.write(updated)
+        let product = model.search('id',id);
+        return product
+    }
+}
+
+module.exports = model
+
+
+/*const model = {
     list: () => JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json'))),
     show: id => model.list().find(e => e.id == id),
     store: data => {
@@ -13,4 +54,5 @@ const model = {
     }
 }
 
-module.exports = model
+module.exports = model 
+*/

ae51a6420939bb6d48a36765b14e1a0ccf15dcb2
Lucas Goñi <lucas.rgoni@gmail.com>
2022-02-12 23:32:43 -0300

    Updating product's forms

--- src/Controllers/products.js
+++ src/Controllers/products.js
@@ -1,67 +1,51 @@
-const product = require('../models/product')
-const file = require('../models/file')
+const product = require("../models/product");
+const file = require("../models/file");
 
 const controller = {
-    index: (req,res) => res.render('products/list',{ // Este metodo es para renderizar el archivo (o la vista) list. Tambien podemos agregar en este mismo metodo
-                                                     // archivos css para que le den forma a la vista list. Podemos agregar todos los css que querramos aunque
-                                                     // por ejemplo para css que vayan a estar en todas las vistas como el header y el footer
-                                                     // tiene mas sentido incluirlos directamente en los archivos html o css que ponerlos en cada metodo de cada controlador
-    styles: ['main'],
-    title: 'Productos',
-    products: product.all().map(p => Object({...p, image: file.search('id',p.image)}) )
+  index: (req, res) =>
+    res.render("products/list", {
+      styles: ["main"],
+      title: "Productos",
+      products: product
+        .all()
+        .map((p) => Object({ ...p, image: file.search("id", p.image) })),
     }),
-    create: (req,res) => res.render('products/create',{
-        styles:['products/create'],
-        title: 'Nuevo Producto',
-        
+  create: (req, res) =>
+    res.render("products/create", {
+      styles: ["products/create"],
+      title: "Nuevo Producto",
     }),
-    save: (req,res) => {
-        req.body.file = req.files
-        let created = product.create(req.body);
-        return res.redirect('/products/'+created.id)
-    },
-    show: (req,res) => {
-        let result = product.search('id', req.params.id)
-        return result ? res.render('products/detail',{
-            styles: ['products/detail'],
-            title: 'Producto | '+result.name,
-            product: result
-        } ) :res.render('error',{
-            msg: 'Producto no encontrado'
+  save: (req, res) => {
+    req.body.file = req.files;
+    let created = product.create(req.body);
+    return res.redirect("/products/" + created.id);
+  },
+  show: (req, res) => {
+    let result = product.search("id", req.params.id);
+    return result
+      ? res.render("products/detail", {
+          styles: ["products/detail"],
+          title: "Producto | " + result.name,
+          product: result,
         })
-    },
-    update: (req,res) => res.render('products/update',{
-        styles: ['products/create'],
-        title: 'Actualizar',
-        product: product.search('id', req.params.id)
+      : res.render("error", {
+          msg: "Producto no encontrado",
+        });
+  },
+  update: (req, res) =>
+    res.render("products/update", {
+      styles: ["products/create"],
+      title: "Actualizar",
+      product: product.search("id", req.params.id),
     }),
-    modify: (req, res) => {
-        let updated = product.update(req.params.id,req.body)
-        return res.redirect('/products/'+created.id)
-    },
-    delete: (req,res) => {
-        product.delete(req.body.id)
-        return res.redirect('/products/')
-    }
+  modify: (req, res) => {
+    let updated = product.update(req.params.id, req.body);
+    return res.redirect("/products/" + created.id);
+  },
+  delete: (req, res) => {
+    product.delete(req.body.id);
+    return res.redirect("/products/");
+  },
+};
 
-}
-
-module.exports = controller
-
-
-
-
-
-
-
-
-/*const controller = {
-    index: (req,res) => res.render ('products/list',{list: product.list()}),
-    create: (req, res) => res.render('products/create'),
-    save:(req, res) => {
-        let result = product.store(req.body);
-        return res.send(result) 
-}
-}
-module.exports = controller
-*/
+module.exports = controller;

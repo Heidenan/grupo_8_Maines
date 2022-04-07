9f4d796c7eac16c0425ce66620b54f2d3621d2b6
Lucas Goñi <lucas.rgoni@gmail.com>
2022-04-07 14:38:21 -0300

    [FIX] Views

--- DashBoardReact/src/components/App.js
+++ DashBoardReact/src/components/App.js
@@ -1,28 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
+import Products from "./Products.js";
+import Users from "./Users.js";
 import cors from "cors";
 /* import {} from "@mui/material"; */
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
 
 class App extends Component {
   // A class component must have its constructor --> allows to receive props (sate)
@@ -30,26 +11,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     // super --> capturates the props of the parent component
-    this.state = {
-      users: [],
-    };
+    this.state = {};
   }
 
-  componentDidMount() {
-    fetch("http://localhost:3000/api/users/last")
-      .then((response) => response.json())
-      .then((response) => this.setState({ users: response.users}));
-  }
+  componentDidMount() {}
 
   componentDidUpdate() {}
 
   render() {
-    if(!this.state.users.length > 0) return null;
-    console.log(this.state.users)
     return (
       <>
-        <h1>{this.props.title}</h1>;
-        {this.state.users.map(user => <h1 key={user.id}>{user.name}</h1>)}
+        <h1>{this.props.title}</h1>
+        <Users />
+        <Products />
       </>
     );
   }
